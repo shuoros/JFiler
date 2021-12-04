4ee811ccc8c9d28c32013ad1f1abd9e443a5423b
Soroush <s_shemshadi@ymail.com>
2021-12-04 12:43:12 +0330

    [ADD] getFile API

--- src/main/java/io/github/shuoros/jfiler/JFiler.java
+++ src/main/java/io/github/shuoros/jfiler/JFiler.java
@@ -1,4 +1,25 @@
 package io.github.shuoros.jfiler;
 
+import io.github.shuoros.jfiler.file.File;
+import io.github.shuoros.jfiler.file.Folder;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class JFiler {
+
+    private Folder location;
+
+    public JFiler(){
+    }
+
+    public JFiler(Path location) throws IOException {
+        this.location = new Folder(location);
+    }
+
+    public File getFile(String location) throws IOException {
+        return new File(Paths.get(location));
+    }
+
 }
