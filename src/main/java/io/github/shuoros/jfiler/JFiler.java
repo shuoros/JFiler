94b0286dde3db17ca01ac3b020bbe289c15c7767
Soroush <s_shemshadi@ymail.com>
2021-12-05 11:34:35 +0330

    [ADD] getFolder API

--- src/main/java/io/github/shuoros/jfiler/JFiler.java
+++ src/main/java/io/github/shuoros/jfiler/JFiler.java
@@ -14,12 +14,16 @@ public class JFiler {
     public JFiler(){
     }
 
-    public JFiler(Path location) throws IOException {
-        this.location = new Folder(location);
+    public JFiler(String location) throws IOException {
+        this.location = new Folder(Paths.get(location));
     }
 
     public File getFile(String location) throws IOException {
         return new File(Paths.get(location));
+    }
+
+    public Folder getFolder(String location) throws IOException {
+        return new Folder(Paths.get(location));
     }
 
 }
--- src/main/java/io/github/shuoros/jfiler/file/Folder.java
+++ src/main/java/io/github/shuoros/jfiler/file/Folder.java
@@ -2,15 +2,27 @@ package io.github.shuoros.jfiler.file;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
-public class Folder extends File{
+public class Folder extends File {
 
-    private List<File> contains;
+    private List<File> contains = new ArrayList<>();
 
     public Folder(Path location) throws IOException {
         super(location);
         this.setType(Type.Folder);
+        Arrays.stream(Objects.requireNonNull(super.list())).forEach(i -> {
+            i = location.toString() + '/' + i;
+            try {
+                contains.add(new java.io.File(i).isFile() ? new File(Paths.get(i)) : new Folder(Paths.get(i)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public List<File> getContains() {
