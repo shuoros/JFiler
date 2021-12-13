3067aee8482e2dfad527fb4ec3c7f097dd270326
Soroush <s_shemshadi@ymail.com>
2021-12-13 10:55:11 +0330

    [IMPLEMENT] folder traversing

--- src/main/java/io/github/shuoros/jfiler/JFiler.java
+++ src/main/java/io/github/shuoros/jfiler/JFiler.java
@@ -6,16 +6,44 @@ import io.github.shuoros.jfiler.file.Folder;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Stack;
 
 public class JFiler {
 
-    private Folder currentlocation;
+    private Boolean lock;
+    private Folder currentLocation;
+    private final Folder homeLocation;
+    private final Stack<Folder> frontLocation = new Stack<>();
+    private final Stack<Folder> rearLocation = new Stack<>();
 
-    public JFiler(){
+    public JFiler() {
+        this("/", false);
     }
 
-    public JFiler(String location) throws IOException {
-        this.currentlocation = new Folder(Paths.get(location));
+    public JFiler(String location) {
+        this(location, false);
+    }
+
+    public JFiler(String location, Boolean lock) {
+        this.homeLocation = new Folder(Paths.get(location));
+        this.currentLocation = this.homeLocation;
+        this.lock = lock;
+    }
+
+    public static JFiler open(String location){
+        return new JFiler(location);
+    }
+
+    public static JFiler openInLockedHome(String location){
+        return new JFiler(location, true);
+    }
+
+    public Folder getCurrentLocation(){
+        return currentLocation;
+    }
+
+    public List<File> getList() {
+        return currentLocation.getContains();
     }
 
     public File getFile(String location) throws IOException {
@@ -26,8 +54,25 @@ public class JFiler {
         return new Folder(Paths.get(location));
     }
 
-    public List<File> getMe(){
-        return currentlocation.getContains();
+    public void openFolder(String location) {
+        this.frontLocation.clear();
+        this.rearLocation.push(this.currentLocation);
+        this.currentLocation = new Folder(Paths.get(location));
+    }
+
+    public void goBackward(){
+        this.frontLocation.push(this.currentLocation);
+        this.currentLocation = this.rearLocation.pop();
+    }
+
+    public void goForward(){
+        this.rearLocation.push(this.currentLocation);
+        this.currentLocation = this.frontLocation.pop();
+    }
+
+    public void goUp(){
+        this.frontLocation.push(this.currentLocation);
+        this.currentLocation = this.currentLocation.getParentFolder();
     }
 
 }
