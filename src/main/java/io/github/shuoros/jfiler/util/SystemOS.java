e64a00b7ea4d85d3d48c5a0f42d2b1f001a988f7
Soroush <s_shemshadi@ymail.com>
2021-12-21 10:02:55 +0330

    [IMPLEMENT] system os class

---
+++ src/main/java/io/github/shuoros/jfiler/util/SystemOS.java
@@ -0,0 +1,25 @@
+package io.github.shuoros.jfiler.util;
+
+public class SystemOS {
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return (OS.contains("win"));
+    }
+
+    public static boolean isMac() {
+        return (OS.contains("mac"));
+    }
+
+    public static boolean isUnix() {
+        return (OS.contains("nix")
+                || OS.contains("nux")
+                || OS.contains("aix"));
+    }
+
+    public static boolean isSolaris() {
+        return (OS.contains("sunos"));
+    }
+
+}
